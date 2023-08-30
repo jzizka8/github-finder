bc06646600b7618c37d5d5f81116dc1391b69691
Josef Zizka <josefzizka21@gmail.com>
2023-08-30 19:23:13 +0200

    feat: MUI + tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { Button, TextField } from '@mui/material';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
+    <div className='flex  items-center'>
+      <h1>Github finder</h1>
+      <div className='text-red-500'>
+        hei
 
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <TextField id="usernameTxt" label="Username" variant="standard" />
+      <Button variant="contained">Find!</Button>
+    </div>
   )
 }
 
