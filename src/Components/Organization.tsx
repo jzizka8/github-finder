9458540b6b50a2ad29967643d610aabc1e9b5289
Josef Zizka <josefzizka21@gmail.com>
2023-09-04 19:56:27 +0200

    fix: weird spacing, too big space for avatar in Organization component

--- src/Components/Organization.tsx
+++ src/Components/Organization.tsx
@@ -7,12 +7,12 @@ interface IOrganizationProps {
 }
 export const Organization = (props: IOrganizationProps) => {
     return (
-        <Card className="grid grid-cols-2 grid-rows-2 my-2 items-center">
+        <Card className="grid grid-cols-[minmax(auto,3rem)_1fr] gap-x-2 grid-rows-2 my-2 items-center px-4 py-2">
             <Avatar alt={props.login}
                 src={props.avatar_url}
-                className="row-span-2" />
-            <Typography variant="h5" component="h4" > {props.login}</Typography>
-            <Typography variant="body1"  > {props.description}</Typography>
+                className="row-span-2 " />
+            <Typography variant="h5" component="h4"  > {props.login}</Typography>
+            <Typography variant="body1" className="text-gray-600"  > {props.description}</Typography>
 
         </Card>
     )
