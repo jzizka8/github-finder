dfb3aa79d2318bf8a6bd157f2a1443efe2da0fd2
Josef Zizka <josefzizka21@gmail.com>
2023-09-04 19:56:42 +0200

    feat: header

---
+++ src/Components/Header.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material"
+
+export const Header = () => {
+    return (
+        <header>
+            <Typography variant="h2" component="h1" className='my-4 text-emerald-700'>Github Finder</Typography>
+        </header>
+    )
+}
