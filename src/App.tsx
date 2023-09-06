2cd77fc8003fff543cb8e801271245b0895aaa92
Josef Zizka <josefzizka21@gmail.com>
2023-09-06 16:59:14 +0200

    fix: min height of the app

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { GithubUserInfo } from './Pages/GithubUserInfo';
 
 function App() {
   return (
-    <div className='flex items-center flex-col bg-zinc-100 px-4'>
+    <div className='flex items-center flex-col bg-zinc-100 px-4 min-h-screen'>
       <Header />
       <GithubUserInfo />
     </div>
